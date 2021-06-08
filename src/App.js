4b2fc108f8e10f1aee46127248bd0b95c333b8e0
Rome Bell <romebell5@gmail.com>
2021-06-08 08:12:55 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// Imports
+import React, { useEffect, useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+// import setAuthToken from './utils/setAuthToken';
+// CSS
+import './App.css';
+// Components
+import Welcome from './components/Welcome';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Profile from './components/Profile';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import About from './components/About';
+
+function App() {
+  // Set state values
+
+  return (
+    <div className="App">
+      {/* <Navbar  /> */}
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Signup.js
@@ -0,0 +1,21 @@
+// Imports
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+const Signup = () => {
+    
+    return (
+        <div className="row mt-4">
+            <div className="col-md-7 offset-md-3">
+                <div className="card card-body">
+                    <h2 className="py-2">Signup</h2>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
